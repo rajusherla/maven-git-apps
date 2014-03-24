4cb3236ee88bb70f8d2edbf96359333c1b08a1b1
rajus <raju.sai12@gmail.com>
2014-03-24 07:24:22 -0500

    initial commit

---
+++ SampleMvnGitApp/src/main/java/com/bizlab/mvn/App.java
@@ -0,0 +1,20 @@
+package com.bizlab.mvn;
+
+/**
+ * Hello world!
+ * 
+ */
+public class App {
+
+	public String getName(final String name) {
+		return name;
+	}
+
+	public int getAddition(final int number1, final int number2) {
+		return number1 + number2;
+	}
+
+	public int getMulitplication(final int number1, final int number2) {
+		return number1 * number2;
+	}
+}
